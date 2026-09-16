357f1e2b6f1ae4c0174bed6c74f596f44ea1dcae
Kiril Devops <211359927+kirildevops@users.noreply.github.com>
2026-09-16 13:31:55 +0300

    fetch with effect p1

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,14 @@
+import { useState, useEffect } from 'react';
+
 const App = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchProducts = async () => {};
+  }, []);
+
   return <div>Shopping Cart UI</div>;
 };
 
