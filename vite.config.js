04ad8d63a573f93a5d0c6e6792e20abd800d6586
Kiril Devops <211359927+kirildevops@users.noreply.github.com>
2026-09-17 10:18:05 +0300

    proxy w/ vite

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
-import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+
+const removePrefix = (str, prefix) =>
+  str.startsWith(prefix) ? str.slice(prefix.length) : str
+
+const removeApiPrefix = (path) => removePrefix(path, '/api')
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-});
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        rewrite: removeApiPrefix,
+      },
+    },
+  },
+})
