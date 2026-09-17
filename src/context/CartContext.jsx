57771a8160b645166802bf75712f39cfc3dad935
Kiril Devops <211359927+kirildevops@users.noreply.github.com>
2026-09-17 18:34:26 +0300

    add cart context p2

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,9 +1,24 @@
-import { createContext, useState, useEffect, useContext } from 'react'
+import { createContext, useState, useContext } from 'react'
 
 const CartContext = createContext()
 
 export function CartProvider({ children }) {
-  return <CartContext value={{}}>{children}</CartContext>
+  const [cart, setCart] = useState([])
+
+  const addToCart = (product) => {
+    setCart((prev) => {
+      const existing = prev.find((item) => item.id === product.id)
+
+      if (existing) {
+        return prev.map((item) =>
+          item.id === product.id ? { ...item, qty: item.qty + 1 } : item,
+        )
+      }
+      return [...prev, { ...product, qty: 1 }]
+    })
+  }
+
+  return <CartContext value={{ cart, addToCart }}>{children}</CartContext>
 }
 
 // eslint-disable-next-line react-refresh/only-export-components
